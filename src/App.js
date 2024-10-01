c9b12cd00298d5f363040c4bef480dbbccaaf7e1
Kornidesz Máté <dev.profile.km@gmail.com>
2024-10-01 14:56:28 +0200

    temporary site created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import logo from './logo.svg';
+import github from './images/github.png';
+import cv from './images/cv.png';
+import construction from './images/construction_sign.png';
+
+import Link from './components/link/Link';
+
 import './App.css';
+import Title from './components/link/title/Title';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my Introduction Website, I am Kornidesz Máté
+        <Title text="Introduction - Kornidesz Máté" />
+        <img src={construction} className="App-logo" alt="logo" />
+        <p className="construction-text">
+          This site is currently under construction, the time until the new site is created, please use my GitHub profile and CV as information source about me.
         </p>
       </header>
+      <main className='App-header'>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Link link="https://github.com/Kornimate" image={github} title="Kornidesz Máté's GitHub profile"/>
+              </td>
+              <td>
+                <Link link="https://drive.google.com/file/d/1Yr6AVcPTLEY9y2rUrAEnMeEvuS8gvZqG/view?usp=sharing" image={cv} title="Kornidesz Máté's CV"/>
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
